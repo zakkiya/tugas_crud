2ae0c1b89b14b09045c37cafbfc5e067d0529258
zakkiya <fnszakkiya@gmail.com>
2022-04-24 10:55:32 +0700

    menambahkan halaman tambah

---
+++ halamantambah.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>tambahrekap</title>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <link rel="stylesheet" type="text/css" href="tambah.css">
+</head>
+<body>
+    <div class="desktop4">
+        <p class="ttmbh">TAMBAH REKAP</p>
+        <p class="tfromtambah" >FROM TAMBAH REKAP PENGUNJUNG PERPUSTAKAAN DESA PADANGAN KABUPATEN BOJONEGORO</p>
+        <br><br>
+        <br><br>
+        <br><br>
+        <br><br>
+        <br><br>
+        <br><br>
+        <br><br>
+        <br><br>
+        <div class="w-50 mx-auto mt-2">
+                <form action="action.php" method="post" class=" border p-3 mt-3">
+                        <label>Nama : </label>
+                        <input type="text" name="nama" class="form-control"><br>
+                        <label>Umur : </label>
+                        <input type="text" name="umur" class="form-control"><br>
+                        <label>Alamat : </label>
+                        <input type="text" name="alamat" class="form-control"><br>
+                        <label>Tglmasuk: </label>
+                        <input type="text" name="tglmasuk" class="form-control"><br>
+                        <input type="submit" class="ktambah">
+                </form>
+</p>
+</div>
+    </div>
+	</body>
+</html>
