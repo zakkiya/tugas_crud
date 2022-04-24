d534e6832a40abd2fac852ec8a641ef5d4d278cf
zakkiya <fnszakkiya@gmail.com>
2022-04-24 11:02:44 +0700

    menamahkan file welcome.php

---
+++ welcome.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>welcome</title>
+ <link rel="stylesheet" type="text/css" href="welcome.css">
+</head>
+<body>
+    <div class="bgwelcome"></div>
+    <p class="selamat">Selamat Datang di Perpustakaan Desa Padangan Kabupaten Bojonegoro</p>
+    <p class="bglibrary">
+    <img src="library.jpg"></p>
+    <p class="tujuan">Tujuan utama pembentukan Perpustakaan Desa adalahsebagai satu sarana untuk meningkatkan pengetahuan dan kemampuan membaca
+         guna mencerdaskan kehidupan masyarakat desa. Oleh karena itu dibutuhkan juga sistem untuk 
+         merekap pengunjung yang datang ke perpustakaan untuk kebutuhan tertentu. </p>
+    <button type="submit"class="tombollogin"><a href="halamanlogin.php"><h1>NEXT</h1></button></a>
+
+
+
+<?php 
+
+?>
+</body>
+</html>
